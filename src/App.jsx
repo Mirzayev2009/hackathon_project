25c0cc002bf6fecb6a025eaf6233d8ef8d2949a4
MirzayevAlisher077 <MirzayevAlisher078@gmail.com>
2025-10-17 18:47:48 +0300

    not

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/HomePage'
-import NotFound from './pages/NotFound'
+import NotFound from  './pages/NotFound'
+
+
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="*" element={<NotFound/>} />
+      <Route path="*" element={<NotFound />} />
+   
     </Routes>
   )
 }
